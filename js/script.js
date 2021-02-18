2ebdde08b25096a99364978352c2b65b39021dd3
lucio rendine <luciorendine69@gmail.com>
2021-02-18 23:58:54 +0100

    push

--- js/script.js
+++ js/script.js
@@ -1,10 +1,17 @@
 var app = new Vue({
  el: '#root',
  data: {
-
+   dropdownVoices:[
+     { voice: 'Home' },
+     { voice: 'Meet the band' },
+     { voice: 'Live Dates' },
+     { voice: 'Latest News' },
+     { voice: 'Albums' },
+     { voice: 'Fans' }
+   ]
  },
  method:{
-   
+
  }
 });
 Vue.config.devtools = true;
