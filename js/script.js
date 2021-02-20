93fd6527e5699c025554cd7bbfd42dbd3b8a2404
lucio rendine <luciorendine69@gmail.com>
2021-02-20 18:36:29 +0100

    push

--- js/script.js
+++ js/script.js
@@ -9,6 +9,18 @@ var app = new Vue({
      { voice: 'Albums' },
      { voice: 'Fans' }
    ],
+   copyrightVoices:[
+     { voice: '© Copyright 2012 - 2021' },
+     { voice: '|AVADA THEME BY THEMEFUSION|' },
+     { voice: 'ALL RIGHTS RESERVED|' },
+     { voice: 'POWERED BY WORDPRESS' },
+   ],
+   socialVoices:[
+     { voice: 'fab fa-facebook-f' },
+     { voice: 'fab fa-twitter' },
+     { voice: 'fab fa-instagram' },
+     { voice: 'fab fa-youtube' },
+   ],
    titles:[
       'Taking it back to the old school',
       'Sharing the stage with a legend',
@@ -17,6 +29,7 @@ var app = new Vue({
       'It just sound better',
       'The flavor of rock'
    ],
+   commonParagraph:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum finibus cursus egestas. Cras quis vehicula quam, a tempus velit. Ut feugiat augue in sodales luctus',
    paragraphCard:'Sed sit amet sem turpis. Curabitur cursus lacinia est at interdum risus id condimentum',
    videoParagraph:'“I just listened to it and said to myself, “Man, I really love this <br> album.” Still, today, it just sounds so fresh. It sounds full of ideas. These guys knew what they were doing. They’re good. And they’re inventive. I haven’t heard anything this year that’s as inventive. I don’t really expect to.”',
    opacityList:[
