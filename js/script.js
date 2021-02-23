d0465340b2879e5ce1095ad9a27624573d82ebdb
lucio rendine <luciorendine69@gmail.com>
2021-02-23 17:21:37 +0100

    push

--- js/script.js
+++ js/script.js
@@ -41,19 +41,10 @@ var app = new Vue({
    commonParagraph:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum finibus cursus egestas. Cras quis vehicula quam, a tempus velit. Ut feugiat augue in sodales luctus',
    paragraphCard:'Sed sit amet sem turpis. Curabitur cursus lacinia est at interdum risus id condimentum',
    videoParagraph:'“I just listened to it and said to myself, “Man, I really love this <br> album.” Still, today, it just sounds so fresh. It sounds full of ideas. These guys knew what they were doing. They’re good. And they’re inventive. I haven’t heard anything this year that’s as inventive. I don’t really expect to.”',
-   opacityList:[
-     '0',
-     '1'
-   ],
-   opacityIdx:0
  },
+ active: true,
  method:{
-   dropdownOn: function() {
-    this.opacityIdx += 1;
-  },
-  dropdownOff: function() {
-   this.opacityIdx -= 1;
- }
+
  }
 });
 Vue.config.devtools = true;
